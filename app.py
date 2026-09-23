fdc01a8be5ef0b9c9f14a5b582944a0dfda08d8d
Shynes2313 <shynes608@gmail.com>
2026-09-24 00:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, UploadFile, File, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
 import shutil
 import os
 
-app = FastAPI()
+app = FastAPI(title="Web Media Converter")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return Response(status_code=204)
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
